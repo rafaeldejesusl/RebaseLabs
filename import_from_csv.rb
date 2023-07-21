42dc3ad4cdc8bce0ebe5e68759cb640ed9d6806d
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2023-07-21 19:18:43 -0300

    Refatoração da tabela

--- import_from_csv.rb
+++ import_from_csv.rb
@@ -2,19 +2,24 @@ require 'csv'
 require 'pg'
 
 conn = PG.connect( host: 'pgserver', dbname: 'rebaselabs', user: 'docker', password: 'docker' )
-conn.exec('CREATE TABLE clients (cpf text, nome_paciente text,
-  email_paciente text, data_nascimento_paciente date, endereco_paciente text,
-  cidade_paciente text, estado_paciente text, crm_medico text,
-  crm_medico_estado text, nome_medico text, email_medico text,
-  token_resultado_exame text, data_exame date, tipo_exame text,
-  limites_tipo_exame text, resultado_tipo_exame text);'
+conn.exec('CREATE TABLE clients (id serial primary key, cpf text, patient_name text,
+  patient_email text, patient_birth_date date, patient_address text,
+  patient_city text, patient_state text, doctor_crm text,
+  doctor_crm_state text, doctor_name text, doctor_email text,
+  exam_result_token text, exam_date date, exam_type text,
+  limits_exam_type text, result_exam_type text);'
 )
 rows = CSV.read("./data.csv", col_sep: ';')
 
 columns = rows.shift
 
 rows.map do |row|
-  conn.exec("INSERT INTO clients VALUES ('#{row[0]}', '#{row[1]}', '#{row[2]}', '#{row[3]}',
+  conn.exec("INSERT INTO clients(cpf, patient_name,
+    patient_email, patient_birth_date, patient_address,
+    patient_city, patient_state, doctor_crm,
+    doctor_crm_state, doctor_name, doctor_email,
+    exam_result_token, exam_date, exam_type,
+    limits_exam_type, result_exam_type) VALUES ('#{row[0]}', '#{row[1]}', '#{row[2]}', '#{row[3]}',
     '#{row[4]}', '#{row[5].gsub("'", "''")}', '#{row[6]}', '#{row[7]}', '#{row[8]}', '#{row[9]}', '#{row[10]}', '#{row[11]}',
     '#{row[12]}', '#{row[13]}', '#{row[14]}', '#{row[15]}')"
   )
